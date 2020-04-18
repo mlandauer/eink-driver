fe1e0a423193e4ac61b76ca93c4e7741234f2035
Matthew Landauer <mlandauer@gmail.com>
2020-04-19 06:02:22 +1000

    Extract env variable and function

--- Dockerfile
+++ Dockerfile
@@ -22,4 +22,5 @@ RUN cd web; /usr/local/go/bin/go mod download
 COPY web/main.go web/
 RUN cd web; /usr/local/go/bin/go build main.go
 
+ENV URL http://192.168.5.10/solar
 CMD ["web/main"]
--- web/main.go
+++ web/main.go
@@ -15,42 +15,35 @@ import (
 	"golang.org/x/image/bmp"
 )
 
-func main() {
-	// create context
-	ctx, cancel := chromedp.NewContext(context.Background())
-	defer cancel()
-
+func screenshotAndDisplay(ctx context.Context, url string) error {
 	var buf []byte
 	// Capture screenshot of a page at a particular browser size
 	// Note we can't use multicast DNS to use the nice name solar.local because
 	// it doesn't work inside a docker container. So, using a hardcoded IP for the time
 	// being. This IP at least is made to be static on the dhcp server (router)
-	if err := chromedp.Run(ctx, fixedSizeScreenshot(`http://192.168.5.10/solar`, &buf)); err != nil {
-		log.Fatal(err)
+	if err := chromedp.Run(ctx, fixedSizeScreenshot(url, &buf)); err != nil {
+		return err
 	}
 	// Convert from png to bmp
 	reader := bytes.NewReader(buf)
 	image, err := png.Decode(reader)
 	if err != nil {
-		log.Fatal(err)
+		return err
 	}
 	// Create a file for writing
 	file, err := os.Create("screenshot.bmp")
 	if err != nil {
-		log.Fatal(err)
+		return err
 	}
 	err = bmp.Encode(file, image)
 	if err != nil {
-		log.Fatal(err)
+		return err
 	}
 	file.Close()
 
 	// Now run the external command to display the image on the eink screen
 	cmd := exec.Command("IT8951/IT8951", "0", "0", "screenshot.bmp")
-	err = cmd.Run()
-	if err != nil {
-		log.Fatal(err)
-	}
+	return cmd.Run()
 }
 
 func fixedSizeScreenshot(urlstr string, res *[]byte) chromedp.Tasks {
@@ -59,5 +52,17 @@ func fixedSizeScreenshot(urlstr string, res *[]byte) chromedp.Tasks {
 		chromedp.EmulateViewport(800, 600),
 		chromedp.Sleep(1 * time.Second),
 		chromedp.CaptureScreenshot(res),
+	}
+}
+
+func main() {
+	url := os.Getenv("URL")
+	// create context
+	ctx, cancel := chromedp.NewContext(context.Background())
+	defer cancel()
+
+	err := screenshotAndDisplay(ctx, url)
+	if err != nil {
+		log.Fatal(err)
 	}
 }
